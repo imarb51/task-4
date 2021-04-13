9a2e67badde374b645f99d9caf1fd569587ab39e
imarb51 <80340473+imarb51@users.noreply.github.com>
2021-04-13 20:17:54 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,10 @@
+def task4(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+print(task4('Mississippi'))
